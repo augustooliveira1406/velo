21109cf80623e418dfe79e8103a8ff19cb2957d3
Augusto Oliveira <augustooliveira@MacBook-Pro-de-Augusto.local>
2026-02-16 11:20:48 -0300

    refactor IA-1

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,14 +1,10 @@
 import { test, expect } from '@playwright/test';
+
 import { generateOrderNumber } from '../support/helpers';
 
+/// AAA - Arrange, Act, Assert
 test.describe('Consultar Pedido', () => {
-  //  hooks
-  // test.beforeAll(async () => {
-  //   console.log(
-  //   'beforeAll: roda uma vez antes de todos os testes.'
-  //   )
-  // })
-  
+
   test.beforeEach(async ({page}) => {
     // Arrange
     await page.goto('http://localhost:5173/');
@@ -18,19 +14,6 @@ test.describe('Consultar Pedido', () => {
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
     })
   
-  // test.afterEach(async () => {
-  //   console.log(
-  //   'afterEach: roda depois de cada teste.'
-  //   )
-  // })
-  
-  // test.afterAll(async () => {
-  //   console.log(
-  //   'afterAll: roda uma vez depois de todos os testes.'
-  //   )
-  // })
- 
-
   test('deve consultar o pedido aprovado', async ({ page }) => {
 
     //Test Data
@@ -80,8 +63,9 @@ test.describe('Consultar Pedido', () => {
     - img
     - paragraph: Pedido
     - paragraph: ${order.number}
-    - img
-    - text: ${order.status}
+    - status:
+      - img
+      - text: ${order.status}
     - img "Velô Sprint"
     - paragraph: Modelo
     - paragraph: Velô Sprint
@@ -105,31 +89,14 @@ test.describe('Consultar Pedido', () => {
     - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
     `);
 
-  
-  });
-  
-  test('deve exibir a mensagem de erro quando o pedido nao for encontrado', async ({ page }) => {
-  
-    //Test Data
-    const orderNumber = generateOrderNumber();
+    const statusBadge = page.getByRole('status').filter({hasText: order.status})
 
-    // Act
-    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(orderNumber);  
-    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
-  
-    // Assert
-    const titleMessage = page.getByRole('heading', { name: 'Pedido não encontrado' });
-    await expect(titleMessage).toBeVisible();
-  
-    const errorMessage = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' });
-    await expect(errorMessage).toBeVisible();
-  
-    //Outra abordagem via Snapshot
-    await expect(page.locator('#root')).toMatchAriaSnapshot(`
-      - img
-      - heading "Pedido não encontrado" [level=3]
-      - paragraph: Verifique o número do pedido e tente novamente
-      `);
+    await expect(statusBadge).toHaveClass(/bg-green-100/)
+    await expect(statusBadge).toHaveClass(/text-green-700/)
+
+    const statusIcon = statusBadge.locator('svg')
+    await expect(statusIcon).toHaveClass(/lucide-circle-check-big/)
+
   
   });
 
@@ -163,8 +130,9 @@ test.describe('Consultar Pedido', () => {
     - img
     - paragraph: Pedido
     - paragraph: ${order.number}
-    - img
-    - text: ${order.status}
+    - status:
+      - img
+      - text: ${order.status}
     - img "Velô Sprint"
     - paragraph: Modelo
     - paragraph: Velô Sprint
@@ -188,6 +156,105 @@ test.describe('Consultar Pedido', () => {
     - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
     `);
 
+    const statusBadge = page.getByRole('status').filter({hasText: order.status})
+
+    await expect(statusBadge).toHaveClass(/bg-red-100/)
+    await expect(statusBadge).toHaveClass(/text-red-700/)
+
+    const statusIcon = statusBadge.locator('svg')
+    await expect(statusIcon).toHaveClass(/lucide-circle-x/)
+
+  });
+
+  test('deve consultar o pedido Em Analise', async ({ page }) => {
+
+    //Test Data
+      const order = {
+      number: 'VLO-22YDBE',
+      status: 'EM_ANALISE',
+      color: 'Glacier Blue',
+      wheels: 'sport Wheels',
+      customer: {
+        name: 'Zaya Blue Shad',
+        email: 'zayabull@gmail.com'
+      },
+      payment: 'À Vista'
+    }
+    
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number);  
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  
+    // Assert
+    await expect(page.getByText('Pedido', { exact: true })).toBeVisible({ timeout: 10_000 });
+    await expect(page.getByText(order.number)).toBeVisible();
+  
+    await expect(page.getByText(order.status)).toBeVisible();
+  
+  
+  await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+    - img
+    - paragraph: Pedido
+    - paragraph: ${order.number}
+    - status:
+      - img
+      - text: ${order.status}
+    - img "Velô Sprint"
+    - paragraph: Modelo
+    - paragraph: Velô Sprint
+    - paragraph: Cor
+    - paragraph: ${order.color}
+    - paragraph: Interior
+    - paragraph: cream
+    - paragraph: Rodas
+    - paragraph: ${order.wheels}
+    - heading "Dados do Cliente" [level=4]
+    - paragraph: Nome
+    - paragraph: ${order.customer.name}
+    - paragraph: Email
+    - paragraph:  ${order.customer.email}
+    - paragraph: Loja de Retirada
+    - paragraph
+    - paragraph: Data do Pedido
+    - paragraph: /\\d+\\/\\d+\\/\\d+/
+    - heading "Pagamento" [level=4]
+    - paragraph:  ${order.payment}
+    - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+    `);
+
+    const statusBadge = page.getByRole('status').filter({hasText: order.status})
+
+    await expect(statusBadge).toHaveClass(/bg-amber-100/)
+    await expect(statusBadge).toHaveClass(/text-amber-700/)
+
+    const statusIcon = statusBadge.locator('svg')
+    await expect(statusIcon).toHaveClass(/lucide-clock/)
   
   });
+
+  test('deve exibir a mensagem de erro quando o pedido nao for encontrado', async ({ page }) => {
+  
+    //Test Data
+    const orderNumber = generateOrderNumber();
+
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(orderNumber);  
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  
+    // Assert
+    const titleMessage = page.getByRole('heading', { name: 'Pedido não encontrado' });
+    await expect(titleMessage).toBeVisible();
+  
+    const errorMessage = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' });
+    await expect(errorMessage).toBeVisible();
+  
+    //Outra abordagem via Snapshot
+    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+      - img
+      - heading "Pedido não encontrado" [level=3]
+      - paragraph: Verifique o número do pedido e tente novamente
+      `);
+  
+  });
+
 });
